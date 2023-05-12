686b364b0f394be8c74518299345f107e311d9bd
Eyram Michael <Eyram.Hlorgbe@Hubtel.com>
2023-05-12 10:49:47 +0000

    Replace toComposeAngle function name with toComposeDegreeAngle

--- loaders/src/main/java/io/eyram/loaders/Utils.kt
+++ loaders/src/main/java/io/eyram/loaders/Utils.kt
@@ -8,7 +8,7 @@ package io.eyram.loaders
  * Turns out, rotations in Figma are done differently compared to what we have in Android.
  * [Read more here](https://help.figma.com/hc/en-us/articles/360039956914-Adjust-alignment-rotation-and-position#Rotation)
  * */
-fun Float.toComposeAngle() = when {
+fun Float.toComposeDegreeAngle() = when {
     this < 0F -> this * -1F
     else -> 360F - this
 }
