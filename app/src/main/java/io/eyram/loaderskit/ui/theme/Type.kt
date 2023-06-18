7a8effffb4eb8eb75b7ad194ed9d0e58f234c832
Eyram Michael <Eyram.Hlorgbe@Hubtel.com>
2023-06-18 10:14:03 +0000

    Add google fonts

--- app/src/main/java/io/eyram/loaderskit/ui/theme/Type.kt
+++ app/src/main/java/io/eyram/loaderskit/ui/theme/Type.kt
@@ -3,32 +3,49 @@ package io.eyram.loaderskit.ui.theme
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
+import io.eyram.loaderskit.R
 
 // Set of Material typography styles to start with
+val fontsDotGoogle = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = io.eyram.loaders.R.array.com_google_android_gms_fonts_certs
+)
+
+val AbrilFatFace = GoogleFont("Abril Fatface")
+val IBMPlexMono = GoogleFont("IBM Plex Mono")
+
+
+val AbrilFontFamily = FontFamily(
+    Font(googleFont = AbrilFatFace, fontProvider = fontsDotGoogle)
+)
+
+val IBMPlexFontFamily = FontFamily(
+    Font(googleFont = IBMPlexMono, fontProvider = fontsDotGoogle)
+)
+
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayLarge = TextStyle(
+        fontFamily = AbrilFontFamily,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
+    displaySmall = TextStyle(
+        fontFamily = IBMPlexFontFamily,
+        fontWeight = FontWeight.Light
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = IBMPlexFontFamily,
+        fontWeight = FontWeight.Light
+    ),
+
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = IBMPlexFontFamily,
+        fontWeight = FontWeight.Light
     )
-    */
 )
