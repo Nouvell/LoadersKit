b8df2f3988f1ec222d85123ba3848d042d0d604b
Eyram Michael <Eyram.Hlorgbe@Hubtel.com>
2023-06-26 13:17:00 +0000

    Make status bar icons light in color.

--- app/src/main/java/io/eyram/loaderskit/ui/theme/Theme.kt
+++ app/src/main/java/io/eyram/loaderskit/ui/theme/Theme.kt
@@ -10,15 +10,18 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    background = Color(0xFF1F1F1F)
 )
 
 private val LightColorScheme = lightColorScheme(
@@ -49,14 +52,16 @@ fun LoadersKitTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
+
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+            val activity = view.context as Activity
+            activity.window.statusBarColor = colorScheme.background.toArgb()
+            WindowCompat.getInsetsController(activity.window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
