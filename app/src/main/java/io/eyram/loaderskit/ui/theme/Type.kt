e9dd7957899257a7466f47c52b9c2a0641d76cd9
Eyram Michael <Eyram.Hlorgbe@Hubtel.com>
2023-06-26 18:15:52 +0000

    Take out Sacramento typeface

--- app/src/main/java/io/eyram/loaderskit/ui/theme/Type.kt
+++ app/src/main/java/io/eyram/loaderskit/ui/theme/Type.kt
@@ -3,12 +3,10 @@ package io.eyram.loaderskit.ui.theme
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.googlefonts.Font
 import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
-import io.eyram.loaderskit.R
 
 // Set of Material typography styles to start with
 val fontsDotGoogle = GoogleFont.Provider(
@@ -17,35 +15,31 @@ val fontsDotGoogle = GoogleFont.Provider(
     certificates = io.eyram.loaders.R.array.com_google_android_gms_fonts_certs
 )
 
-val AbrilFatFace = GoogleFont("Abril Fatface")
 val IBMPlexMono = GoogleFont("IBM Plex Mono")
 
-
-val AbrilFontFamily = FontFamily(
-    Font(googleFont = AbrilFatFace, fontProvider = fontsDotGoogle)
-)
-
 val IBMPlexFontFamily = FontFamily(
-    Font(googleFont = IBMPlexMono, fontProvider = fontsDotGoogle)
+    Font(
+        googleFont = IBMPlexMono,
+        fontProvider = fontsDotGoogle
+    )
 )
-
 
 val Typography = Typography(
-    displayLarge = TextStyle(
-        fontFamily = AbrilFontFamily,
-        fontWeight = FontWeight.Normal,
-    ),
+
     displaySmall = TextStyle(
         fontFamily = IBMPlexFontFamily,
-        fontWeight = FontWeight.Light
+        fontWeight = FontWeight.Light,
+        fontSize = 36.sp
     ),
     headlineSmall = TextStyle(
         fontFamily = IBMPlexFontFamily,
-        fontWeight = FontWeight.Light
+        fontWeight = FontWeight.Light,
+        fontSize = 24.sp
     ),
 
     labelSmall = TextStyle(
         fontFamily = IBMPlexFontFamily,
-        fontWeight = FontWeight.Light
+        fontWeight = FontWeight.Light,
+        fontSize = 11.sp
     )
 )
