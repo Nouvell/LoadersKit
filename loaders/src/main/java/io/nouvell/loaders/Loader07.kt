e5ff5857229bb2cea6257fe0fa1f008c92e88226
Eyram Michael <Eyram.Hlorgbe@Hubtel.com>
2023-07-08 18:26:02 +0000

    Implement loader07

--- loaders/src/main/java/io/nouvell/loaders/Loader07.kt
+++ loaders/src/main/java/io/nouvell/loaders/Loader07.kt
@@ -1,16 +1,14 @@
 package io.nouvell.loaders
 
-import androidx.compose.animation.core.EaseIn
+import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.EaseInOut
-import androidx.compose.animation.core.EaseOut
-import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.StartOffset
+import androidx.compose.animation.core.StartOffsetType
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.keyframes
 import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -18,107 +16,119 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.graphics.drawscope.DrawStyle
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
-import io.nouvell.loaders.util.ANIMATION_TIME as DEFAULT_ANIMATION_TIME
+import io.nouvell.loaders.color.LoaderColor
 import io.nouvell.loaders.util.Dimens
-import kotlinx.coroutines.delay
-import kotlin.math.min
+import kotlin.math.roundToInt
 
 @Composable
 fun Loader07(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    color: LoaderColor = LoaderColor.Rainbow,
 ) {
+    val colorList = remember(color) { color.getColors() }
+    val transition = rememberInfiniteTransition("${TAG}InfiniteTransition")
 
-    val brush = remember { SolidColor(Color.White) }
-    val density = LocalDensity.current
+    val animatedProperties = List(3) { index ->
+        val radiusScale by transition.animateFloat(
+            initialValue = 0.0625F,
+            targetValue = 0.0625F,
+            animationSpec = infiniteRepeatable(
+                animation = keyframes {
+                    durationMillis = ANIMATION_TIME
+                    0.0625F atFraction 0.111F with EaseInOut
 
-    val transition = rememberInfiniteTransition("${TAG} InfiniteTransition")
+                    0.75F atFraction 0.333F
+                    0.75F atFraction 0.444F with EaseInOut
 
-    val alphaTransition by transition.animateFloat(
-        initialValue = 0f,
-        targetValue = 0f,
+                    1F atFraction 0.667F
+                    1F atFraction 0.778F with EaseInOut
+
+                    0.0625F atFraction 1F
+                },
+                initialStartOffset = StartOffset(
+                    offsetType = StartOffsetType.FastForward,
+                    offsetMillis = (ANIMATION_TIME * index / 3F).roundToInt()
+                )
+            )
+        )
+
+        val alpha by transition.animateFloat(
+            initialValue = 0F,
+            targetValue = 0F,
+            animationSpec = infiniteRepeatable(
+                animation = keyframes {
+                    durationMillis = ANIMATION_TIME
+                    0F atFraction 0.111F with EaseInOut
+
+                    1F atFraction 0.333F
+                    1F atFraction 0.444F with EaseInOut
+
+                    0F atFraction 0.667F
+                    0F atFraction 1F
+                },
+                initialStartOffset = StartOffset(
+                    offsetType = StartOffsetType.FastForward,
+                    offsetMillis = (ANIMATION_TIME * index / 3F).roundToInt()
+                )
+            )
+        )
+
+        val strokeScale by transition.animateFloat(
+            initialValue = 0.0078F,
+            targetValue = 0.1250F,
+            animationSpec = infiniteRepeatable(
+                animation = keyframes {
+                    durationMillis = ANIMATION_TIME
+                    0.0078F atFraction 0.111F with EaseInOut
+
+                    0.0625F atFraction 0.333F
+                    0.0625F atFraction 0.444F with EaseInOut
+
+                    0.1250F atFraction 0.667F
+                    0.1250F atFraction 1F
+                },
+                initialStartOffset = StartOffset(
+                    offsetType = StartOffsetType.FastForward,
+                    offsetMillis = (ANIMATION_TIME * index / 3F).roundToInt()
+                )
+            )
+        )
+
+        Triple(radiusScale, alpha, strokeScale)
+    }
+
+
+    val loaderColor by transition.animateColor(
+        initialValue = colorList.first(),
+        targetValue = colorList.last(),
         animationSpec = infiniteRepeatable(
             animation = keyframes {
                 durationMillis = ANIMATION_TIME
-                0f atFraction 0f
-                1f atFraction 0.4f with EaseIn
-                0f atFraction 0.5f with EaseOut
-                0f atFraction 0.51f
-                0f atFraction 1f
+                colorList.forEachIndexed { index, color ->
+                    color atFraction ((index + 1).toFloat() / colorList.size)
+                }
             },
         ),
-        label = "$TAG AlphaTransition"
-    )
-
-    val radiusTransition by transition.animateFloat(
-        initialValue = 0f,
-        targetValue = 0f,
-        animationSpec = infiniteRepeatable(
-            animation = keyframes {
-                durationMillis = ANIMATION_TIME
-                0.3f atFraction 0f
-                0.8f atFraction 0.4f with EaseIn
-                1f atFraction 0.5f with EaseOut
-                1f atFraction 1f
-            }
-        ),
-        label = "$TAG AlphaTransition"
-    )
-
-    val alphaTransition2 by transition.animateFloat(
-        initialValue = 0f,
-        targetValue = 0f,
-        animationSpec = infiniteRepeatable(
-            animation = keyframes {
-                durationMillis = ANIMATION_TIME
-
-                0f atFraction 0f
-                0f atFraction 0.4f
-                0.4f atFraction 0.5f
-                1f atFraction 0.8f with EaseInOut
-                0f atFraction 1f with EaseOut
-            },
-        ),
-        label = "$TAG AlphaTransition2"
-    )
-
-    val radiusTransition2 by transition.animateFloat(
-        initialValue = 0f,
-        targetValue = 0f,
-        animationSpec = infiniteRepeatable(
-            animation = keyframes {
-                durationMillis = ANIMATION_TIME
-                0f atFraction 0f
-                0f atFraction 0.4f
-                0.3f atFraction 0.5f with EaseIn
-                0.8f atFraction 0.8f with EaseOut
-                1f atFraction 1f
-            }
-        ),
-        label = "$TAG AlphaTransition2"
+        label = "ColorAnimation"
     )
 
 
     Canvas(
-        modifier = modifier.size(80.dp)
+        modifier = modifier
+            .size(Dimens.DEFAULT_LOADER_SIZE)
     ) {
-        drawCircle(
-            brush = SolidColor(Color.Black.copy(alpha = alphaTransition)),
-            radius = (min(size.height, size.width) / 2) * radiusTransition,
-            style = Stroke(width = with(density) { 5.dp.toPx() })
-        )
-
-        drawCircle(
-            brush = SolidColor(Color.Black.copy(alpha = alphaTransition2)),
-            radius = (min(size.height, size.width) / 2) * radiusTransition2,
-            style = Stroke(width = with(density) { 5.dp.toPx() })
-        )
+        animatedProperties.forEach { (radiusScale, alpha, strokeScale) ->
+            drawCircle(
+                color = loaderColor,
+                radius = size.minDimension / 2 * radiusScale,
+                style = Stroke(size.minDimension * strokeScale),
+                alpha = alpha
+            )
+        }
     }
 }
 
-
 private const val TAG = "Loader07"
-private const val ANIMATION_TIME = (DEFAULT_ANIMATION_TIME) //* 0.8f).toInt()
+private const val ANIMATION_TIME = 2700
